fa73d2f93614e44e85d6e17a15cb227a0be6863f
Feror <contact@feror.fr>
2025-03-20 20:36:19 +0100

    Ajout de ExecutionEnvironment

--- src/services/getAdequateModifier.ts
+++ src/services/getAdequateModifier.ts
@@ -1,6 +1,12 @@
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
 const getAdequateModifier = () => {
-	// Return "Ctrl" if the user is on Windows or Linux, and ⌘ if the user is on macOS
-	return navigator.userAgent.indexOf("Mac") != -1 ? "⌘" : "Ctrl + ";
+	if (ExecutionEnvironment.canUseDOM) {
+		// Return "Ctrl" if the user is on Windows or Linux, and ⌘ if the user is on macOS
+		return navigator.userAgent.indexOf("Mac") != -1 ? "⌘" : "Ctrl + ";
+	} else {
+		return "Ctrl";
+	}
 };
 
 export default getAdequateModifier;
